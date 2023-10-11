3e3abc0e77bde72cf4a2f81b4b2cde739ca3db5e
jeanlol0123 <jeanpaulol0123@gmail.com>
2023-10-11 14:14:16 -0500

    Version_incrustacion HTML

--- shMessages/proyecto/ia/views.py
+++ shMessages/proyecto/ia/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render
-from .forms import textAreaForm
-from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_protect
+from django.http import JsonResponse
+from .phishing__email import load_or_train_model, preprocess_text, predict_phishing
 
-
+# Carga el modelo y el vectorizador al inicio del módulo
+model, vectorizer = load_or_train_model()
 # Create your views here.
+@csrf_protect
 def main(request):
-    return render(request, 'main_page.html')
-
-def envioEmailPish(request):
     if request.method == 'POST':
-        form = textAreaForm(request.POST)
-
-        if form.is_valid():
-            text_content = form.cleaned_data['text_area_content']
-
-           
-
-            return HttpResponse(f"Text entered: {text_content}")
-
-    else:
-        form = textAreaForm()
-
-    return render(request, 'main_page.html', {'form': form})
+        user_input = request.POST['email_text']
+        if user_input:
+            # Cargar o entrenar el modelo
+            
+            
+            # Preprocesar el texto
+            processed_input = preprocess_text(user_input)
+            
+            # Realizar la predicción
+            result = predict_phishing(processed_input, model, vectorizer)
+            
+            return JsonResponse({'result': int(result)})
+    
+    return render(request, 'main_page.html')
